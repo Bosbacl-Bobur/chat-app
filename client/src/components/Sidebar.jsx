cc9dbfba6d915b83b495013acbc8ea290dcbbadc
Bosbacl-Bobur <bosbacl713@gmail.com>
2025-09-01 22:10:19 +0500

    add

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
     const navigate = useNavigate()
     
   return (
-    <div className={`bg-[#8185B2]/10 w-[300px] h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ''}`}>
+    <div className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ''}`}>
       <div className='pb-5 '>
         <div className='flex justify-between items-center'>
             <img src={assets.logo} alt="logo" className='max-w-40' />
